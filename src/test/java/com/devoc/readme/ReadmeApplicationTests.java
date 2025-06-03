c230ce89436cfe052c6f5321a4ce22858b0bf8b0
kharbaqossama <ossamakharbaq4@gmail.com>
2025-06-03 19:58:49 +0100

    fixing pipeline issue

--- src/test/java/com/devoc/readme/ReadmeApplicationTests.java
+++ src/test/java/com/devoc/readme/ReadmeApplicationTests.java
@@ -2,12 +2,13 @@ package com.devoc.readme;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.properties")
+@Import(TestSecurityConfig.class)
 class ReadmeApplicationTests {
 	@Test
 	void contextLoads() {

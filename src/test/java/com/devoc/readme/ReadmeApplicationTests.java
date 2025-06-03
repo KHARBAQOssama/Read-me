a30beedf272cc2aac2b21911dcda7831a88bd275
kharbaqossama <ossamakharbaq4@gmail.com>
2025-06-03 16:03:51 +0100

    fixing pipeline issue

--- src/test/java/com/devoc/readme/ReadmeApplicationTests.java
+++ src/test/java/com/devoc/readme/ReadmeApplicationTests.java
@@ -1,11 +1,13 @@
 package com.devoc.readme;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class ReadmeApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
